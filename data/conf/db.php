0bba48ac4c50e3cff25498753b82ff459f3caa5e
温柔如斯 <1145769693@qq.com>
2015-02-27 13:31:51 +0800

    测试添加提交thinkcmfx的文件

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'thinkcmfx',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'sp_',
+    //密钥
+    "AUTHCODE" => '7BY2M222r2TCIEeV77',
+    //cookies
+    "COOKIE_PREFIX" => 'U49dZ1_',
+);
+?>
