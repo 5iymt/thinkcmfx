514c6f8301143fd43917121e379bccaeeee20b08
温柔如斯 <1145769693@qq.com>
2015-02-28 16:58:08 +0800

    Signed-off-by: 温柔如斯 <1145769693@qq.com>

--- data/conf/db.php
+++ data/conf/db.php
@@ -5,15 +5,15 @@
  */
 return array(
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => 'localhost',
+    'DB_HOST' => '127.0.0.1',
     'DB_NAME' => 'thinkcmfx',
     'DB_USER' => 'root',
     'DB_PWD' => '',
     'DB_PORT' => '3306',
     'DB_PREFIX' => 'sp_',
     //密钥
-    "AUTHCODE" => '7BY2M222r2TCIEeV77',
+    "AUTHCODE" => '2t3kg6MyybnWbyFaCA',
     //cookies
-    "COOKIE_PREFIX" => 'U49dZ1_',
+    "COOKIE_PREFIX" => 'SasfTQ_',
 );
 ?>
